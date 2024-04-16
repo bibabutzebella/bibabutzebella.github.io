7a90b7fbcb8066a995f6e6ee8de91bcc55a389fc
bibabutzebella <isabellajaensch@googlemail.com>
2024-04-16 21:13:40 +0200

    Initial commit

---
+++ js/context_menu.js
@@ -0,0 +1,37 @@
+function openContextMenu(menuText, linkUrl) {
+    var popup = document.createElement('div');
+    popup.className = 'popup';
+
+    // Erstelle das Element für den Menütext
+    var menuTextElement = document.createElement('div');
+    menuTextElement.textContent = menuText;
+    menuTextElement.className = 'popup-menu-text';
+    popup.appendChild(menuTextElement);
+
+    // Füge Link zum Popup-Fenster hinzu, falls vorhanden
+    if (linkUrl) {
+        var link = document.createElement('a');
+        link.textContent = 'Hier gibt\'s mich';
+        link.href = linkUrl;
+        link.className = 'popup-link-text';
+        popup.appendChild(link);
+    }
+
+    // Close-Button hinzufügen
+    var closeButton = document.createElement('button');
+    closeButton.textContent = 'X';
+    closeButton.className = 'popup-close-button'; // Füge die neue Klasse hinzu
+    closeButton.onclick = function() {
+        document.body.removeChild(popup);
+    };
+    popup.appendChild(closeButton);
+
+    // Positioniere das Popup-Fenster in der Mitte des Bildschirms
+    popup.style.position = 'fixed';
+    popup.style.top = '50%';
+    popup.style.left = '50%';
+    popup.style.transform = 'translate(-50%, -50%)';
+
+    // Füge das Popup-Fenster zum Body hinzu
+    document.body.appendChild(popup);
+}
